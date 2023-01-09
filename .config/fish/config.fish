965786e145346034cef5adac54b18ef5cea331ce
Riey <creeper844@gmail.com>
2023-01-09 17:12:50 +0900

    Add fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,13 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+export EDITOR=nvim
+
+export GTK_IM_MODULE=kime
+export GTK2_IM_MODULE=xim
+export QT_IM_MODULE=kime
+export XMODIFIER=@im=kime
+
+export GDK_BACKEND=wayland
+
