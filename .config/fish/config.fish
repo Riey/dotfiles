b3f84f963172e631fc44e4725d69b720f1ce8431
Riey <creeper844@gmail.com>
2023-07-29 16:42:01 +0900

    Update config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,16 +1,5 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    set -g fish_greeting
 end
-
-export EDITOR=nvim
-
-export GTK_IM_MODULE=kime
-export GTK2_IM_MODULE=xim
-export QT_IM_MODULE=kime
-export XMODIFIER=@im=kime
-
-export PICO_SDK_PATH=/opt/pico-sdk
-export GDK_BACKEND=wayland
-export MOZ_ENABLE_WAYLAND=1
-export PATH="$PATH:$HOME/.cargo/bin"
 
